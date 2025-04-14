612535d2d078a4303e2e6b15084c22271416770f
rMiccolis <r.miccolis1@gmail.com>
2025-04-14 21:31:15 +0200

    fix tls-cert secret name

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # setting variables for tls certificate
-KEY_FILE="tls.key"
-CERT_FILE="tls.crt"
+KEY_FILE="tls-cert.key"
+CERT_FILE="tls-cert.crt"
 HOST="$app_server_addr"
 #   cert_file_name='tls-cert'
 # create a certificate for https protocol
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}" -addext "subjectAltName = DNS:${HOST}"
 
-# create a tls.pem file from combination of $CERT_FILE and $KEY_FILE
-cat $KEY_FILE $CERT_FILE > tls.pem
-kubectl create secret generic tls --from-file=${CERT_FILE}=tls.pem -n mongodb
+# create a tls-cert.pem file from combination of $CERT_FILE and $KEY_FILE
+cat $KEY_FILE $CERT_FILE > tls-cert.pem
+kubectl create secret generic tls-cert --from-file=${CERT_FILE}=tls-cert.pem -n mongodb
 
 # use mongod.conf file to tell mongodb to use it for configuration (it contains tls certificates)
 # store this mongod.conf containing configuration for mongo inside a configmap that will be loaded inside the container
