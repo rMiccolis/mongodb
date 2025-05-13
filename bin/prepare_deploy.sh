080ad1a25a96ab708c2032b6a21ba29274fc17af
rMiccolis <r.miccolis1@gmail.com>
2025-05-13 19:19:07 +0200

    first try with bitnami helm chart

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -3,12 +3,20 @@
 
 # setting variables for tls certificate
 KEY_FILE="tls-cert.key"
-CERT_FILE="tls-cert.crt"
+CERT_FILE="mongodb.pem"
 
 # setting secret for tls certificate
 kubectl create secret generic tls-cert --from-file=${CERT_FILE}=$repository_root_dir/tls/tls-cert.pem -n mongodb
+kubectl create secret generic mongodb-ca \
+  --from-file=ca.crt=/home/apps/mongodb/tls/ca.crt \
+  -n mongodb
 
 # use mongod.conf file to tell mongodb to use it for configuration (it contains tls certificates)
 # store this mongod.conf containing configuration for mongo inside a configmap that will be loaded inside the container
 
 kubectl create configmap mongodb-config --from-file=$repository_root_dir/apps/mongodb/res/mongod.conf -n mongodb
+
+
+helm install mongodb bitnami/mongodb \
+  -f values.yaml \
+  -n mongodb
