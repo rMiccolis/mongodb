8f5a5291825d53ec9f0373ea81dee8998899688c
rMiccolis <r.miccolis1@gmail.com>
2025-05-14 00:26:22 +0200

    tls working but client connection

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -6,11 +6,11 @@ KEY_FILE="tls-cert.key"
 CERT_FILE="mongodb.pem"
 
 # setting secret for tls certificate
-awk '/-----BEGIN PRIVATE KEY-----/,/-----END PRIVATE KEY-----/' $repository_root_dir/tls/tls-cert.pem > mongodb-ca-key.pem
-awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' $repository_root_dir/tls/tls-cert.pem > mongodb-ca-cert.pem
+awk '/-----BEGIN PRIVATE KEY-----/,/-----END PRIVATE KEY-----/' $repository_root_dir/tls/tls-cert.pem > $repository_root_dir/tls/mongodb-ca-key.pem
+awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' $repository_root_dir/tls/tls-cert.pem > $repository_root_dir/tls/mongodb-ca-cert.pem
 
 kubectl -n mongodb create secret generic mongodb-ca-secret \
-    --from-file=mongodb-ca-cert=./mongodb-ca-cert.pem \
-    --from-file=mongodb-ca-key=./mongodb-ca-key.pem
+    --from-file=mongodb-ca-cert=$repository_root_dir/tls/mongodb-ca-cert.pem \
+    --from-file=mongodb-ca-key=$repository_root_dir/tls/mongodb-ca-key.pem
 
 helm install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb -n mongodb -f $repository_root_dir/apps/mongodb/res/values.yaml
