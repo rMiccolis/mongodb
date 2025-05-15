5d691ac9c8c51a82734cdd88f87106f916f147e9
rMiccolis <r.miccolis1@gmail.com>
2025-05-15 14:35:39 +0200

    fix use of pvc before creating it

--- bin/finalize_deploy.sh
+++ bin/finalize_deploy.sh
@@ -1,3 +1,22 @@
+# setting variables for tls certificate
+KEY_FILE="tls-cert.key"
+CERT_FILE="mongodb.pem"
+
+# setting secret for tls certificate
+mkdir $repository_root_dir/tls/mongodb
+awk '/-----BEGIN PRIVATE KEY-----/,/-----END PRIVATE KEY-----/' $repository_root_dir/tls/tls-cert.pem > $repository_root_dir/tls/mongodb/mongodb-ca-key.pem
+awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' $repository_root_dir/tls/tls-cert.pem > $repository_root_dir/tls/mongodb/mongodb-ca-cert.pem
+
+kubectl -n mongodb create secret generic mongodb-ca-secret \
+    --from-file=mongodb-ca-cert=$repository_root_dir/tls/mongodb/mongodb-ca-cert.pem \
+    --from-file=mongodb-ca-key=$repository_root_dir/tls/mongodb/mongodb-ca-key.pem
+
+helm install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb -n mongodb -f $repository_root_dir/apps/mongodb/env_subsitution/values.yaml
+
+kubectl rollout status deployment mongodb -n $namespace --timeout=3000s > /dev/null 2>&1
+kubectl wait --for=condition=Ready --all pods --all-namespaces --timeout=3000s &
+wait
+
 mongodb_podname=$(kubectl get pods -n mongodb | awk 'NR==2{print $1}')
 
 kubectl -n mongodb cp ${mongodb_podname}:/certs/mongodb.pem ${repository_root_dir}/tls/mongodb/mongodb.pem
